0043343c45c943a509a08948a25435203afdce63
Pablo H. Paladino <paladinop@gmail.com>
2016-07-15 21:38:05 -0400

    chart detalle

--- ganancias-empresas/app/scripts/main.js
+++ ganancias-empresas/app/scripts/main.js
@@ -1,14 +1,24 @@
 var CHQ;
 
-;(function(global, document, $, Tabletop){
+;(function(global, document, $, Tabletop, Mustache){
 
     'use strict';
     CHQ = {};
 
     CHQ.pymChild = pym.Child({ polling: 500 });
 
+    CHQ.$details = $('#details');
+
+    CHQ.$body =  $('body'); 
+
+    CHQ.$colChart = $('#col-chart-container');
+
+    CHQ.circles;
+
     CHQ.init = function(){
-        console.log('init');
+        var w = CHQ.$body.width();
+        CHQ.$body.css('min-height',(w*9)/16);
+
         Tabletop.init( { key: '1lZH4R70Ln3vqZFnDvySuA681kKJJWc49GEoFJwpKLDc',
                    callback: CHQ.dataLoaded,
                    simpleSheet: false,
@@ -44,9 +54,10 @@ var CHQ;
         CHQ.maxValue = d3.max(CHQ.rawData,function(d){
             return d.max;
         });
+        CHQ.$body.removeClass('loading');
         CHQ.filterData('2015');
         CHQ.render();
-        CHQ.startEvents();
+        setTimeout(CHQ.startEvents,1000);
     };
 
     CHQ.filterData = function(y){
@@ -57,40 +68,70 @@ var CHQ;
     };
 
     CHQ.startEvents = function(){
-        /*$(window).resize(function() {
+
+        CHQ.circles
+            .on('click',function(d){
+                CHQ.openDetails(CHQ.dataById[d.data.id]);
+                d3.selectAll('circle').classed('selected',false);
+                d3.select(this).classed('selected',true);
+                //$('#details').html('BASE: ' + JSON.stringify(d.data)+ ' HISTORICO: '+JSON.stringify(CHQ.dataById[d.data.id]));
+            })
+            .on('mouseenter',function(d){
+              CHQ.tooltip
+                .html(
+                  '<strong>'+d.data.empresa+'</strong> pagó el <strong>'+d.data.porcentaje+'%</strong> en el año <strong>'+d.data.anio+'</strong>'
+                )
+                .style('opacity',1);
+
+                d3.select(this).attr('stroke',d3.rgb(CHQ.color(d.data.sector)).darker().toString());
+            })
+            .on('mousemove', function(){
+                CHQ.tooltip
+                  .style('top',(d3.event.pageY-20)+'px')
+                  .style('left',(d3.event.pageX+20)+'px');
+            })
+            .on('mouseout',function(d){
+                CHQ.tooltip.style('opacity',0);
+                d3.select(this).attr('fill',function(){
+
+                });
+            });
+
+        $(window).resize(function() {
             clearTimeout(CHQ.timeoutId);
             CHQ.timeoutId = setTimeout(CHQ.render, 500);
             
-        });*/
+        });
         $('#year').change(function(){
-            $('#detalle').html('');
             CHQ.filterData($(this).val());
             CHQ.render();
         });
+        $('#close-details').on('click',CHQ.closeDetails);
     };
 
     CHQ.render = function(){
 //        console.log(CHQ.data);
-        console.log(CHQ.groups);
-        console.log(CHQ.categories);
+//        console.log(CHQ.groups);
+//        console.log(CHQ.categories);
 //        console.log(CHQ.maxValue);
 
         var w = $('#chart-container').width();
 
+        var h = (CHQ.$colChart.hasClass('col-md-12'))?(w*9)/16:w;
+
         var width = w,
-            height = (w*9)/16,
+            height = h,
             padding = 1.5, // separation between same-color circles
             clusterPadding = 10, // separation between different-color circles
-            maxRadius = w/20;
+            maxRadius = w/25,
+            minRadius = w/100;
 
-        var color = d3.scale.category20()
+        CHQ.color = d3.scale.category20()
             .domain(CHQ.categories);
-
-            console.log('color',color.domain());
 
         var rScale = d3.scale.linear()
             .domain([0,CHQ.maxValue])
-            .range([0,maxRadius]);
+            .range([minRadius,maxRadius]);
 
 
         // The largest node for each cluster.
@@ -130,45 +171,22 @@ var CHQ;
             .attr('width', width)
             .attr('height', height);
 
-        var circle = CHQ.svg.selectAll('circle')
+        CHQ.circles = CHQ.svg.selectAll('circle')
             .data(nodes);
 
-        circle
+        CHQ.circles
             .enter()
-            .append('circle')
-            .on('click',function(d){
-                $('#detalle').html('BASE: ' + JSON.stringify(d.data)+ ' HISTORICO: '+JSON.stringify(CHQ.dataById[d.data.id]));
-            })
-            .on('mouseenter',function(d){
-              CHQ.tooltip
-                .html(
-                  '<strong>'+d.data.empresa+'</strong> pagó el <strong>'+d.data.porcentaje+'%</strong> en el año <strong>'+d.data.anio+'</strong>'
-                )
-                .style('opacity',1);
+            .append('circle');
 
-                d3.select(this).attr('stroke',d3.rgb(color(d.data.sector)).darker().toString());
-            })
-            .on('mousemove', function(){
-                CHQ.tooltip
-                  .style('top',(d3.event.pageY-20)+'px')
-                  .style('left',(d3.event.pageX+20)+'px');
-            })
-            .on('mouseout',function(d){
-              CHQ.tooltip.style('opacity',0);
-              d3.select(this).attr('fill',function(){
-
-              });
-            });
-
-        circle
+        CHQ.circles
             .attr('r', function(d) { return d.radius; })
-            .style('fill', function(d) { return color(d.cluster); })
+            .style('fill', function(d) { return CHQ.color(d.cluster); })
             .call(force.drag);
 
-        circle.exit().remove();
+        CHQ.circles.exit().remove();
 
         function tick(e) {
-          circle
+          CHQ.circles
               .each(cluster(10 * e.alpha * e.alpha))
               .each(collide(.5))
               .attr('cx', function(d) { return d.x; })
@@ -234,5 +252,96 @@ var CHQ;
         }
     };
 
+    CHQ.openDetails = function(data){
 
-})(window, document, jQuery, Tabletop);
+      if(CHQ.$colChart.hasClass('col-md-12')){
+        CHQ.$colChart.removeClass('col-md-12').addClass('col-md-6');
+        CHQ.render();
+        CHQ.$details.fadeIn();
+      }
+
+      var template = $('#template').html();
+      Mustache.parse(template);   // optional, speeds up future uses
+      data.color = CHQ.color(data.sector);
+      var rendered = Mustache.render(template, data);
+      $('#details-block').html(rendered);
+
+      var years = {};
+
+      $.each(data,function(i,item){
+        if(i.indexOf('_') !== -1){
+          i = i.split('_');
+          years[i[1]] = true;
+        }
+      });
+
+      years = d3.keys(years).sort();
+      
+      var json = [];
+
+      $.each(years,function(ix,year){
+        var obj = {anio:year};
+        $.each(data,function(i,item){
+          if(i.indexOf('_') !== -1){
+            i = i.split('_');
+            if(i[1]==year){
+              obj[i[0]] = item;
+            }
+          }
+        });
+        json.push(obj);
+      });
+
+      var config = {
+              json: json,
+              keys: {
+                x: 'anio',
+                value: ['ganancias', 'ventas','porcentaje'],
+              },
+              axes: {
+                'porcentaje': 'y2'
+              },
+              type: 'line',
+              types:{
+                'porcentaje':'bar'
+              }
+          };
+
+      if(!CHQ.chart){
+        CHQ.chart = c3.generate({
+          bindto: '#line-chart',
+          data: config,
+          bar: {
+            width: {
+              ratio: 0.2
+            }
+          },
+          axis: {
+              y2: {
+                  show: true,
+                  min: 0,
+                  label: 'Porcentaje'
+              },
+              y:{
+                min: 0,
+                label: 'Ganancias / Ventas'
+              },
+              x: {
+                min: 0,
+                type: 'category'
+              }
+          }
+        });
+      } else {
+        CHQ.chart.load(config);
+      }
+    };
+
+    CHQ.closeDetails = function(){
+      CHQ.$colChart.removeClass('col-md-6').addClass('col-md-12');
+      CHQ.render();
+      CHQ.$details.hide();
+    };
+
+
+})(window, document, jQuery, Tabletop, Mustache);
