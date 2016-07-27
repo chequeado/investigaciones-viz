badfd70a2b2af51ff15464cb73c3e2451f460c90
Pablo H. Paladino <paladinop@gmail.com>
2016-07-27 18:52:24 -0400

    filters + legends

--- ganancias-empresas/app/scripts/main.js
+++ ganancias-empresas/app/scripts/main.js
@@ -70,15 +70,221 @@ d3.selection.prototype.moveToFront = function() {
     };
 
     CHQ.filterData = function(){
-        var filtered = CHQ.rawData.filter(function(d){
-            return !isNaN(d['porcentaje_'+CHQ.year]);
+        var filtered = CHQ.rawData.map(function(d){
+          if(isNaN(d['porcentaje_'+CHQ.year])) {
+            d.noPresentaron = true;
+            d['porcentaje_'+CHQ.year] = 0;            
+          }
+          return d;
         });
-        CHQ.data = filtered.map(function(d){return {id:d.id,empresa:d.empresa,sector:d.sector,anio:CHQ.year,porcentaje:d['porcentaje_'+CHQ.year]}});
+        CHQ.data = filtered.map(function(d){return {id:d.id,empresa:d.empresa,sector:d.sector,noPresentaron:d.noPresentaron,anio:CHQ.year,porcentaje:d['porcentaje_'+CHQ.year]}});
     };
 
     CHQ.startEvents = function(){
 
+//        CHQ.circles
+
+
+        $(window).resize(function() {
+            clearTimeout(CHQ.timeoutId);
+            CHQ.timeoutId = setTimeout(CHQ.render, 500);
+            
+        });
+        $('.btn-year').on('click',function(){
+            $('.btn-year').removeClass('btn-selected');
+            $(this).addClass('btn-selected');
+            CHQ.year = $(this).data('value');
+            CHQ.render();
+        });
+        $('.btn-order').on('click',function(){
+            $('.btn-order').removeClass('btn-selected');
+            $(this).addClass('btn-selected');
+            CHQ.group = $(this).data('value');
+            CHQ.render();
+        });
+        $('#close-details').on('click',CHQ.closeDetails);
+    };
+
+    CHQ.render = function(){
+//        console.log(CHQ.data);
+//        console.log(CHQ.groups);
+//        console.log(CHQ.categories);
+//        console.log(CHQ.maxValue);
+
+        CHQ.filterData();
+
+        var w = $('#chart-container').width();
+
+        CHQ.smallDevice = (w < 990);
+
+        var h = (!CHQ.smallDevice && !CHQ.selectedId)?(w*9)/16:w;
+
+        var width = w,
+            height = h,
+            padding = 2, // separation between same-color circles
+            clusterPadding = 2, // separation between different-color circles
+            maxRadius = w/25,
+            minRadius = w/100,
+            max2Radius = w/75,
+            min2Radius = w/150;
+
+        CHQ.color = d3.scale.category20()
+            .domain(CHQ.categories);
+
+        var clusters,nodes,rScale;
+
+        var dataLegend = {
+          max: Math.ceil(CHQ.maxValue),
+          min: 0,
+          minSize:0,
+          maxSize:0,
+          classContainer: (!CHQ.smallDevice && !CHQ.selectedId && !CHQ.group!='center')?'col-md-3':'col-sm-12 col-xs-12',
+          classRef: (!CHQ.smallDevice && !CHQ.selectedId && !CHQ.group!='center')?'col-md-12':'col-sm-4 col-xs-4'
+        };
+
+        switch(CHQ.group){
+          case 'center':
+            rScale = d3.scale.linear()
+              .domain([0,CHQ.maxValue])
+              .range([minRadius,maxRadius]);
+
+            dataLegend.minSize = minRadius*2;
+            dataLegend.maxSize = maxRadius*2;
+
+            // The largest node for each cluster.
+            clusters = new Array(CHQ.categories.length);
+
+            nodes = CHQ.data.map(function(d) {
+              var i = d.sector,
+                  r = rScale(d.porcentaje),
+                  d = {cluster: i, radius: r,data:d};
+                  
+                  if (!clusters[i] || (r > clusters[i].radius)){
+                    clusters[i] = d;
+                  }
+
+              return d;
+            });
+
+            if(CHQ.textGroup){
+              CHQ.textGroup.selectAll('text').style('opacity',0);
+            }
+          break;
+          case 'sector':
+            rScale = d3.scale.linear()
+              .domain([0,CHQ.maxValue])
+              .range([min2Radius,max2Radius]);
+
+            dataLegend.minSize = min2Radius*2;
+            dataLegend.maxSize = max2Radius*2;
+
+            clusters = new Array(CHQ.categories.length);
+            var clusterPoints = new Array(CHQ.categories.length);
+            var textos = [];
+
+            var wCol = Math.floor(width/11),
+                hRow = Math.floor(height/4),
+                cols = d3.range(0,width,wCol),
+                rows = d3.range(0,height,hRow),
+                ixCols = 0,
+                ixRows = 1;
+
+            nodes = CHQ.data.map(function(d) {
+              var i = d.sector,
+                  r = rScale(d.porcentaje),
+                  d = {cluster: i, radius: r,data:d};
+                  
+                  if (!clusterPoints[i]){
+                    if(cols[ixCols+2]){
+                      ixCols += 1;
+                    }else{
+                      ixCols = 1;
+                      ixRows += 1;
+                    }
+                    clusterPoints[i] = {x:cols[ixCols],y:rows[ixRows],radius:3,title:i,anchor:'middle'};
+                    textos.push(clusterPoints[i]);
+                  }
+
+                  if (!clusters[i] || (r > clusters[i].radius)){
+                    clusters[i] = d;
+                  }
+
+              return d;
+            });
+
+            if(!CHQ.smallDevice){
+              //titles
+              d3plus.textBox()
+                .select('g#text-chart-group')
+                .data(textos)
+                .width(wCol)
+                .text(function(d) { return d.title; })
+                .textAnchor(function(d) { return d.anchor; })
+                .x(function(d) { return d.x-(wCol/2); })
+                .y(function(d) { return d.y + max2Radius; })
+                .fontSize(12)
+                ();
+
+              CHQ.textGroup.selectAll('text').style('opacity',1);
+            } else {
+              CHQ.textGroup.selectAll('text').style('opacity',0);
+            }
+
+            //Render select
+            CHQ.renderSelect();
+
+          break;
+        }
+
+        //RenderLegend
+        var template = $('#tpl-legend').html();
+        Mustache.parse(template);
+        
+        dataLegend.midSize = (dataLegend.minSize + dataLegend.maxSize)/2;
+        dataLegend.midPadding = (dataLegend.maxSize - dataLegend.midSize)/2;
+        dataLegend.minPadding = (dataLegend.maxSize - dataLegend.minSize)/2;
+        dataLegend.txtPadding = (dataLegend.maxSize - 20)/2;
+
+        var rendered = Mustache.render(template, dataLegend);
+        $('#legend-container').html(rendered);
+
+        //console.log(nodes);
+        //console.log(clusters);
+
+        var force = d3.layout.force()
+            .nodes(nodes)
+            .size([width, height])
+            .gravity(0)
+            .charge(0)
+            .on('tick', tick)
+            .start();
+
+        if(!CHQ.svg) {
+            CHQ.svg = d3.select('#chart-container').append('svg').attr('id','main-chart-svg');
+            CHQ.circlesGroup = CHQ.svg.append('g').attr('id','circles-chart-group');
+            CHQ.textGroup = CHQ.svg.append('g').attr('id','text-chart-group');
+            
+            CHQ.tooltip = d3.select('body')
+                .append('div')
+                .attr('id','circle-tooltip');
+            CHQ.selector = CHQ.svg.append('circle')
+                .attr('id','selector')
+                .style('stroke-width',3)
+                .style('opacity',0)
+                .attr('fill','none');
+        }
+
+        CHQ.svg
+          .attr('width', width)
+          .attr('height', height);
+
+        CHQ.circles = CHQ.circlesGroup.selectAll('circle.company')
+            .data(nodes);
+
         CHQ.circles
+            .enter()
+            .append('circle')
+            .classed('company',true)
             .on('click',function(d){
                 CHQ.openDetails(CHQ.dataById[d.data.id]);
                 d3.selectAll('circle').classed('selected',false);
@@ -118,191 +324,39 @@ d3.selection.prototype.moveToFront = function() {
             })
             .on('mouseout',function(d){
                 CHQ.tooltip.style('opacity',0);
-                d3.select(this).attr('fill',function(){
-
-                });
             });
-
-        $(window).resize(function() {
-            clearTimeout(CHQ.timeoutId);
-            CHQ.timeoutId = setTimeout(CHQ.render, 500);
-            
-        });
-        $('.btn-year').on('click',function(){
-            $('.btn-year').removeClass('btn-selected');
-            $(this).addClass('btn-selected');
-            CHQ.year = $(this).data('value');
-            CHQ.render();
-        });
-        $('.btn-order').on('click',function(){
-            $('.btn-order').removeClass('btn-selected');
-            $(this).addClass('btn-selected');
-            CHQ.group = $(this).data('value');
-            CHQ.render();
-        });
-        $('#close-details').on('click',CHQ.closeDetails);
-    };
-
-    CHQ.render = function(){
-//        console.log(CHQ.data);
-//        console.log(CHQ.groups);
-//        console.log(CHQ.categories);
-//        console.log(CHQ.maxValue);
-
-        CHQ.filterData();
-
-        var w = $('#chart-container').width();
-
-        CHQ.smallDevice = (w < 992);
-
-        var h = (!CHQ.smallDevice && !CHQ.selectedId)?(w*9)/16:w;
-
-        var width = w,
-            height = h,
-            padding = 2, // separation between same-color circles
-            clusterPadding = 2, // separation between different-color circles
-            maxRadius = w/25,
-            minRadius = w/100,
-            max2Radius = w/75,
-            min2Radius = w/150;
-
-        CHQ.color = d3.scale.category20()
-            .domain(CHQ.categories);
-
-        var clusters,nodes,rScale;
-
-        switch(CHQ.group){
-          case 'center':
-            rScale = d3.scale.linear()
-              .domain([0,CHQ.maxValue])
-              .range([minRadius,maxRadius]);
-            // The largest node for each cluster.
-            clusters = new Array(CHQ.categories.length);
-
-            nodes = CHQ.data.map(function(d) {
-              var i = d.sector,
-                  r = rScale(d.porcentaje),
-                  d = {cluster: i, radius: r,data:d};
-                  
-                  if (!clusters[i] || (r > clusters[i].radius)){
-                    clusters[i] = d;
-                  }
-
-              return d;
-            });
-
-            if(CHQ.titles){
-              CHQ.titles.style('opacity',0);
-            }
-          break;
-          case 'sector':
-            rScale = d3.scale.linear()
-              .domain([0,CHQ.maxValue])
-              .range([min2Radius,max2Radius]);
-
-            clusters = new Array(CHQ.categories.length);
-            var clusterPoints = new Array(CHQ.categories.length);
-            var textos = [];
-
-            var wCol = Math.floor(width/11),
-                hRow = Math.floor(height/4),
-                cols = d3.range(0,width,wCol),
-                rows = d3.range(0,height,hRow),
-                ixCols = 0,
-                ixRows = 1;
-
-            nodes = CHQ.data.map(function(d) {
-              var i = d.sector,
-                  r = rScale(d.porcentaje),
-                  d = {cluster: i, radius: r,data:d};
-                  
-                  if (!clusterPoints[i]){
-                    if(cols[ixCols+2]){
-                      ixCols += 1;
-                    }else{
-                      ixCols = 1;
-                      ixRows += 1;
-                    }
-                    clusterPoints[i] = {x:cols[ixCols],y:rows[ixRows],radius:3,title:i};
-                    textos.push(clusterPoints[i]);
-                  }
-
-                  if (!clusters[i] || (r > clusters[i].radius)){
-                    clusters[i] = d;
-                  }
-
-              return d;
-            });
-
-            if(!CHQ.smallDevice){
-              //titles
-              CHQ.titles = CHQ.svg.selectAll('text.group-title')
-                  .data(textos);
-
-              CHQ.titles.enter()
-                  .append('text')
-                  .classed('group-title',true)
-                  .attr('text-anchor','middle')
-                  
-              CHQ.titles
-                  .text(function(d){ return d.title;})
-                  .attr('x', function(d) { return d.x; })
-                  .attr('y', function(d) { return d.y + max2Radius; })
-                  .style('opacity',1);
-
-              CHQ.titles.exit().remove();
-            } else {
-              CHQ.svg.selectAll('text.group-title').style('opacity',0);
-            }
-
-            //Render select
-            CHQ.renderSelect();
-
-          break;
-        }
-
-        //console.log(nodes);
-        //console.log(clusters);
-
-        var force = d3.layout.force()
-            .nodes(nodes)
-            .size([width, height])
-            .gravity(0)
-            .charge(0)
-            .on('tick', tick)
-            .start();
-
-        if(!CHQ.svg) {
-            CHQ.svg = d3.select('#chart-container').append('svg');
-            CHQ.tooltip = d3.select('body')
-                .append('div')
-                .attr('id','circle-tooltip');
-            CHQ.selector = CHQ.svg.append('circle')
-                .attr('id','selector')
-                .style('stroke-width',3)
-                .style('opacity',0)
-                .attr('fill','none');
-        }
-
-        CHQ.svg
-          .attr('width', width)
-          .attr('height', height);
-
-        CHQ.circles = CHQ.svg.selectAll('circle.company')
-            .data(nodes);
-
-        CHQ.circles
-            .enter()
-            .append('circle')
-            .classed('company',true);
 
         CHQ.circles
             .attr('id',function(d){return 'e'+d.data.id})
-            .attr('r', function(d) { return d.radius; })
-            .style('fill', function(d) { return CHQ.color(d.cluster); })
+            .attr('stroke',function(d){ 
+              if(d.data.noPresentaron){
+                return 'black';
+              }
+              return CHQ.color(d.data.sector);
+            })
+            .classed('zero',function(d){
+              return (d.data.porcentaje == 0)?true:false;
+            })
+            .attr('r', function(d) {
+              if(d.data.noPresentaron){
+                return rScale(0);
+              }
+              return d.radius; 
+            })
+            .style('fill', function(d) { 
+              if(d.data.noPresentaron){
+                return 'black';
+              }
+              if(d.data.porcentaje == 0){
+                return 'white';
+              }
+              return CHQ.color(d.cluster); 
+            })
             .call(force.drag);
 
         CHQ.circles.exit().remove();
+
+        //CHQ.startEvents();
 
         CHQ.selector.moveToFront();
 
@@ -443,7 +497,7 @@ d3.selection.prototype.moveToFront = function() {
       Mustache.parse(template);   // optional, speeds up future uses
       data.color = CHQ.color(data.sector);
       data.selectedYear = CHQ.year;
-      data.isPromedio = (CHQ.year == 'promedio')
+      data.isPromedio = (CHQ.year == 'promedio');
       data.selectedValue = (data['porcentaje_'+CHQ.year]+'').replace('.',',');
       var rendered = Mustache.render(template, data);
       $('#details-block').html(rendered);
@@ -513,7 +567,8 @@ d3.selection.prototype.moveToFront = function() {
             y:{
               label: 'Porcentaje',
               min: 0,
-              show:false
+              show:false,
+              padding: {bottom: 0,top:10}
             },
             x: {
               type: 'category'
@@ -557,7 +612,8 @@ d3.selection.prototype.moveToFront = function() {
                 tick: {
                   //format: d3.format('$,'),
                   format: function (d) { return d3.format('$,')(d).replace(/,/g,'.'); }
-                }
+                },
+                padding: {bottom: 0}
               },
               x: {
                 type: 'category',
@@ -565,14 +621,7 @@ d3.selection.prototype.moveToFront = function() {
                   outer:false
                 }
               }
-          },
-          grid: {
-                y: {
-                  lines: [
-                    {value: 0, text: ' '}
-                  ]
-                }
-              }
+          }
         });
       } else {
         CHQ.chart.load(config);
