9908239bc2723edf7b6d612ef91fa5904694465d
Pablo H. Paladino <paladinop@gmail.com>
2017-03-02 12:37:53 -0300

    mobile + style + axis

--- agroquimicos/app/scripts/controllers/main.js
+++ agroquimicos/app/scripts/controllers/main.js
@@ -49,6 +49,13 @@ angular.module('agroquimicosApp')
         $scope.renderChart();
     });
 
+    var id;
+    $(window).resize(function() {
+        clearTimeout(id);
+        id = setTimeout($scope.renderChart, 500);
+        
+    });
+
     //Questions
     $scope.start = function(){
         $scope.myAnswers = {values:[]};
@@ -56,13 +63,12 @@ angular.module('agroquimicosApp')
         $scope.updateMyCoordinates();
         $scope.currentId = 1;
         $scope.current = $scope.preguntas['pregunta_'+$scope.currentId];
-        $scope.myCircle
+        $scope.myGroup
             .transition()
             .duration(1000)
             .ease("elastic")
-            .attr("r", 10)
-            .attr("cx", function (d,i) { return $scope.x(0); } )
-            .attr("cy", function (d) { return $scope.y(0); } );
+            .attr('opacity',1)
+            .attr('transform', 'translate(' + ($scope.x(0)-6) + ',' + ($scope.y(0)-6) + ')');
 
         $scope.state = 'game';
     };
@@ -105,12 +111,11 @@ angular.module('agroquimicosApp')
 
     $scope.updateMyCoordinates = function(){
         $scope.myCoordinates = $scope.getCoordinates($scope.myAnswers);
-        $scope.myCircle
+        $scope.myGroup
             .transition()
             .duration(1000)
             .ease("elastic")
-            .attr("cx", function (d,i) { return $scope.x($scope.myCoordinates.x); } )
-            .attr("cy", function (d) { return $scope.y($scope.myCoordinates.y); } );
+            .attr('transform', 'translate(' + ($scope.x($scope.myCoordinates.x)-6) + ',' + ($scope.y($scope.myCoordinates.y)-6) + ')');
     };
 
     $scope.getCoordinates = function(e){
@@ -146,7 +151,7 @@ angular.module('agroquimicosApp')
     };
 
     $scope.chart;
-    $scope.myCircle;
+    $scope.myGroup;
     $scope.x;
     $scope.y;
 
@@ -154,10 +159,15 @@ angular.module('agroquimicosApp')
     $scope.renderChart = function(){
         var data = $scope.entrevistas;
 
-        var w = $('#chart-container').width();
+        var w = $('#chart-container').html("").width();
 
         var margin = {top: 25, right: 25, bottom: 25, left: 25}
-            , width = w - margin.left - margin.right
+        console.log($scope.isMobile());
+        if($scope.isMobile()){
+            margin = {top: 0, right: 0, bottom: 20, left: 20}
+        }
+
+        var width = w - margin.left - margin.right
             , height = w - margin.top - margin.bottom;
     
         $scope.x = d3.scale.linear()
@@ -184,6 +194,7 @@ angular.module('agroquimicosApp')
         var layoutGroup = main.append("svg:g").classed("layoutGroup",true);
         var axisGroup = main.append("svg:g").classed("axisGroup",true);
         var dataGroup = main.append("svg:g").classed("dataGroup",true);
+        $scope.myGroup = main.append("svg:g").classed("myGroup",true).attr("opacity",0);
 
                 
         // draw the x axis
@@ -286,12 +297,44 @@ angular.module('agroquimicosApp')
             $scope.hoverCircle.style("opacity",0);
         });
 
-        $scope.myCircle = dataGroup.append("svg:circle")
+        $scope.myGroup.append("svg:circle")
             .attr('class', 'scatter-dots my-dot')
-            .attr("cx", function (d,i) { return $scope.x(0); } )
-            .attr("cy", function (d) { return $scope.y(0); } )
-            .attr("r", 0);
+            .attr("cx", 6)
+            .attr("cy", 6 )
+            .attr("r", 12);
 
+        $scope.myGroup.append("svg:text")
+            .attr('class', 'my-text')
+            .text("VOS")
+            .attr("x", 6)
+            .attr("y", 9)
+            .attr("text-anchor", "middle");
+
+        axisGroup
+            .append("text")
+            .attr("class","axis-label")
+            .attr("x", -height/2)
+            .attr("y", -5)
+            .text("El Estado debe intervenir nada o mucho")
+            .attr("text-anchor", "middle")
+            .attr("transform", "rotate(270)");
+
+        axisGroup
+            .append("text")
+            .attr("class","axis-label")
+            .attr("x", height/2)
+            .attr("y", height+14)
+            .text("Los agroquímicos afectan el medioambiente nada o mucho")
+            .attr("text-anchor", "middle");
+
+    }
+
+    $scope.isMobile = function() {
+        var isMobile = false; //initiate as false
+        // device detection
+        if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
+        || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) isMobile = true;        
+            return isMobile;
     }
 
   });
