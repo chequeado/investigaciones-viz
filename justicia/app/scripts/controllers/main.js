2125289cc8a25f3c88390b25d13b2ef2ae09d7e2
Pablo H. Paladino <paladinop@gmail.com>
2018-04-25 12:38:30 -0300

    responsive

--- justicia/app/scripts/controllers/main.js
+++ justicia/app/scripts/controllers/main.js
@@ -15,7 +15,7 @@ angular.module("justiciaApp").controller("MainCtrl", function($scope) {
       text:
         "Tu denuncia por corrupción es la única de las 219 relevadas que concluyó con una condena firme. En 2016, la Corte Suprema de Justicia de Tucumán confirmó el fallo condenatorio contra el comisario Enrique Antonio García y el policía Manuel Ezequiel Yapura por encubrimiento agravado, y el agente Roberto Oscar Lencina por adulterar un acta en la causa por el asesinato de Paulina Lebbos, una joven tucumana de 23 años.",
       video: "01.mp4",
-      color: "#3ABDC1"
+      color: "#D50001"
     },
     {
       id: 2,
@@ -31,7 +31,7 @@ angular.module("justiciaApp").controller("MainCtrl", function($scope) {
       text:
         "Tu denuncia es una de las 29 que involucran a José Alperovich, exgobernador (2003 - 2015) y senador nacional del Partido Justicialista.  Una lástima: aunque es, por lejos, el funcionario tucumano más denunciado desde 2005, ninguno de los procesos abiertos en su contra prosperó. Por lo que se sabe, Alperovich ni siquiera llegó a prestar una declaración indagatoria ante la Justicia.",
       video: "03.mp4",
-      color: "#D50001"
+      color: "#3ABDC1"
     },
     {
       id: 4,
@@ -45,7 +45,7 @@ angular.module("justiciaApp").controller("MainCtrl", function($scope) {
       id: 5,
       title: "En revisión",
       text:
-        "Sos un ciudadano bastante afortunado. Tu denuncia avanzó y hoy tiene dos condenas. Sin embargo, el fallo está en revisión. Hasta ahora sólo una de las 219 denuncias por corrupción presentadas entre 2005 y 2017 culminó en una condena firme. ¿Tendrás la suerte de conseguir justicia?.",
+        "Sos un ciudadano bastante afortunado. Tu denuncia avanzó y hoy tiene dos condenas. Sin embargo, el fallo está en revisión. Hasta ahora sólo una de las 219 denuncias por corrupción presentadas entre 2005 y 2017 culminó en una condena firme. ¿Tendrás la suerte de conseguir justicia?",
       video: "05.mp4",
       color: "#00769E"
     }
